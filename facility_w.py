96c6dc2589b58a21cd90ec18aa57a95d7d8342f8
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-09-01 20:25:42 +0300

    Update facility_w.py

--- facility_w.py
+++ facility_w.py
@@ -138,28 +138,28 @@ if page == 'Event Logging':
                 </h2>
                 """, unsafe_allow_html=True)
     with col2:
-                search_keyword = st.session_state.get('search_keyword', '')
-                search_keyword = st.text_input("Enter keyword to search:", search_keyword)
-                search_button = st.button("Search")
-                search_option = 'All Columns'
-            
-            def search_in_dataframe(df_Material, keyword, option):
-                if option == 'All Columns':
-                    result = df_Material[df_Material.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
-                else:
-                    result = df_Material[df_Material[option].astype(str).str.contains(keyword, case=False)]
-                return result
-            
-            if st.session_state.get('refreshed', False):
-                st.session_state.search_keyword = ''
-                st.session_state.refreshed = False
-            
-            if search_button and search_keyword:
-                st.session_state.search_keyword = search_keyword
-                search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
-                st.write(f"Search results for '{search_keyword}'in{search_option}:")
-                st.dataframe(search_results, width=1000, height=200)
-            st.session_state.refreshed = True
+        search_keyword = st.session_state.get('search_keyword', '')
+        search_keyword = st.text_input("Enter keyword to search:", search_keyword)
+        search_button = st.button("Search")
+        search_option = 'All Columns'
+    
+    def search_in_dataframe(df_Material, keyword, option):
+        if option == 'All Columns':
+            result = df_Material[df_Material.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
+        else:
+            result = df_Material[df_Material[option].astype(str).str.contains(keyword, case=False)]
+        return result
+    
+    if st.session_state.get('refreshed', False):
+        st.session_state.search_keyword = ''
+        st.session_state.refreshed = False
+    
+    if search_button and search_keyword:
+        st.session_state.search_keyword = search_keyword
+        search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
+        st.write(f"Search results for '{search_keyword}'in{search_option}:")
+        st.dataframe(search_results, width=1000, height=200)
+    st.session_state.refreshed = True
     
     
     image_save_path = 'uploaded_images'

7384f0e14fe556dab4f8da9d9999a108b7ab8a6e
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-09-01 20:24:20 +0300

    Update facility_w.py

--- facility_w.py
+++ facility_w.py
@@ -130,11 +130,37 @@ page = st.sidebar.radio('Select page', ['Event Logging', 'Work Shop Order', 'Vie
 
 if page == 'Event Logging':
     checklist_df = load_checklist_data()
-    st.markdown("""
+    col1, col2 = st.columns([2, 0.75])
+    with col1:
+        st.markdown("""
                 <h2 style='text-align: center; font-size: 40px; color: #A52A2A;'>
                     Facility Maintenance Checklist:
                 </h2>
                 """, unsafe_allow_html=True)
+    with col2:
+                search_keyword = st.session_state.get('search_keyword', '')
+                search_keyword = st.text_input("Enter keyword to search:", search_keyword)
+                search_button = st.button("Search")
+                search_option = 'All Columns'
+            
+            def search_in_dataframe(df_Material, keyword, option):
+                if option == 'All Columns':
+                    result = df_Material[df_Material.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
+                else:
+                    result = df_Material[df_Material[option].astype(str).str.contains(keyword, case=False)]
+                return result
+            
+            if st.session_state.get('refreshed', False):
+                st.session_state.search_keyword = ''
+                st.session_state.refreshed = False
+            
+            if search_button and search_keyword:
+                st.session_state.search_keyword = search_keyword
+                search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
+                st.write(f"Search results for '{search_keyword}'in{search_option}:")
+                st.dataframe(search_results, width=1000, height=200)
+            st.session_state.refreshed = True
+    
     
     image_save_path = 'uploaded_images'
     os.makedirs(image_save_path, exist_ok=True)

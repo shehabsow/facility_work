ec63b7f4eab25fdf492e1f15f80f2cb51ed8ce02
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-09-03 13:26:02 +0300

    Update facility_w.py

--- facility_w.py
+++ facility_w.py
@@ -189,125 +189,125 @@ if page == 'Event Logging':
 ]
 
 # تقسيم القائمة إلى جزئين
-mid_index = len(locations) // 2
-locations_part1 = locations[:mid_index]
-locations_part2 = locations[mid_index:]
-
-# إنشاء تبويبات للجزء الأول
-tabs1 = st.tabs(locations_part1)
-for location, tab in zip(locations_part1, tabs1):
-    with tab:
-        st.subheader(f'{location} Checklist.')
-        for category, items in checklist_items.items():
-            st.markdown(f"<h3 style='color:green; font-size:24px;'>{category}.</h3>", unsafe_allow_html=True)
-            for item in items:
-                st.markdown(f"<span style='color:blue; font-size:18px;'>* {item}</span>", unsafe_allow_html=True)
-
-            col1a, col2a, col3a, col4a = st.columns([1, 2, 2, 2])
-            Event_Detector_Name = col2a.text_input('Detector Name.', key=f"detector_name_{category}_{location}")
-            Rating = col1a.selectbox('Rating.', [0, 1, 2, 3, 'N/A'], key=f"Rating_{category}_{location}")
-            comment = col3a.text_input('comment.', '', key=f"comment_{category}_{location}")
-            responsible_person = col4a.selectbox('Select Responsible Person.', [''] + repair_personnel, key=f"person_{category}_{location}")
-            uploaded_file = st.file_uploader(f"Upload Images {category}", type=["jpg", "jpeg", "png"], key=f"image_{category}_{location}")
-            if st.button(f'Add {category}', key=f"add_{category}_{location}"):
-                # تعيين رقم الحدث بناءً على التقييم
-                if Rating in [0, 'N/A']:
-                    event_id = 'check'
-                else:
-                    event_id = get_next_event_id()
-
-                image_path = ""
-                if uploaded_file is not None:
-                    try:
-                        image = Image.open(uploaded_file)
-                        if image.mode == "RGBA":
-                            image = image.convert("RGB")
-                        max_size = (800, 600)
-                        image.thumbnail(max_size)
-                        image_filename = os.path.join('uploaded_images', f"{event_id}.jpg") if event_id else os.path.join('uploaded_images', f"no_id_{uploaded_file.name}")
-                        image.save(image_filename, optimize=True, quality=85)
-                        image_path = image_filename
-                        st.success(f"Image saved successfully as {uploaded_file.name}")
-                    except Exception as e:
-                        st.error(f"An error occurred while saving the image: {str(e)}")
-                        image_path = ""
-
-                new_row = {
-                    'event id': event_id,
-                    'location': location,
-                    'Element': category,
-                    'Event Detector Name': Event_Detector_Name,
-                    'Date': datetime.now().replace(tzinfo=None),
-                    'Rating': Rating,
-                    'comment': comment,
-                    'responsible person': responsible_person,
-                    'Expected repair Date': '',
-                    'Actual Repair Date': '',
-                    'image path': image_path
-                }
-
-                new_row_df = pd.DataFrame([new_row])
-                st.session_state.checklist_df = pd.concat([st.session_state.checklist_df, new_row_df], ignore_index=True)
-                st.session_state.checklist_df.to_csv('checklist_records.csv', encoding='utf-8', index=False)
-                st.success(f"Event recorded successfully! '{category}'!")
-
-# إنشاء تبويبات للجزء الثاني
-tabs2 = st.tabs(locations_part2)
-for location, tab in zip(locations_part2, tabs2):
-    with tab:
-        st.subheader(f'{location} Checklist.')
-        for category, items in checklist_items.items():
-            st.markdown(f"<h3 style='color:green; font-size:24px;'>{category}.</h3>", unsafe_allow_html=True)
-            for item in items:
-                st.markdown(f"<span style='color:blue; font-size:18px;'>* {item}</span>", unsafe_allow_html=True)
-
-            col1a, col2a, col3a, col4a = st.columns([1, 2, 2, 2])
-            Event_Detector_Name = col2a.text_input('Detector Name.', key=f"detector_name_{category}_{location}")
-            Rating = col1a.selectbox('Rating.', [0, 1, 2, 3, 'N/A'], key=f"Rating_{category}_{location}")
-            comment = col3a.text_input('comment.', '', key=f"comment_{category}_{location}")
-            responsible_person = col4a.selectbox('Select Responsible Person.', [''] + repair_personnel, key=f"person_{category}_{location}")
-            uploaded_file = st.file_uploader(f"Upload Images {category}", type=["jpg", "jpeg", "png"], key=f"image_{category}_{location}")
-            if st.button(f'Add {category}', key=f"add_{category}_{location}"):
-                # تعيين رقم الحدث بناءً على التقييم
-                if Rating in [0, 'N/A']:
-                    event_id = 'check'
-                else:
-                    event_id = get_next_event_id()
-
-                image_path = ""
-                if uploaded_file is not None:
-                    try:
-                        image = Image.open(uploaded_file)
-                        if image.mode == "RGBA":
-                            image = image.convert("RGB")
-                        max_size = (800, 600)
-                        image.thumbnail(max_size)
-                        image_filename = os.path.join('uploaded_images', f"{event_id}.jpg") if event_id else os.path.join('uploaded_images', f"no_id_{uploaded_file.name}")
-                        image.save(image_filename, optimize=True, quality=85)
-                        image_path = image_filename
-                        st.success(f"Image saved successfully as {uploaded_file.name}")
-                    except Exception as e:
-                        st.error(f"An error occurred while saving the image: {str(e)}")
-                        image_path = ""
-
-                new_row = {
-                    'event id': event_id,
-                    'location': location,
-                    'Element': category,
-                    'Event Detector Name': Event_Detector_Name,
-                    'Date': datetime.now().replace(tzinfo=None),
-                    'Rating': Rating,
-                    'comment': comment,
-                    'responsible person': responsible_person,
-                    'Expected repair Date': '',
-                    'Actual Repair Date': '',
-                    'image path': image_path
-                }
-
-                new_row_df = pd.DataFrame([new_row])
-                st.session_state.checklist_df = pd.concat([st.session_state.checklist_df, new_row_df], ignore_index=True)
-                st.session_state.checklist_df.to_csv('checklist_records.csv', encoding='utf-8', index=False)
-                st.success(f"Event recorded successfully! '{category}'!")
+    mid_index = len(locations) // 2
+    locations_part1 = locations[:mid_index]
+    locations_part2 = locations[mid_index:]
+    
+    # إنشاء تبويبات للجزء الأول
+    tabs1 = st.tabs(locations_part1)
+    for location, tab in zip(locations_part1, tabs1):
+        with tab:
+            st.subheader(f'{location} Checklist.')
+            for category, items in checklist_items.items():
+                st.markdown(f"<h3 style='color:green; font-size:24px;'>{category}.</h3>", unsafe_allow_html=True)
+                for item in items:
+                    st.markdown(f"<span style='color:blue; font-size:18px;'>* {item}</span>", unsafe_allow_html=True)
+    
+                col1a, col2a, col3a, col4a = st.columns([1, 2, 2, 2])
+                Event_Detector_Name = col2a.text_input('Detector Name.', key=f"detector_name_{category}_{location}")
+                Rating = col1a.selectbox('Rating.', [0, 1, 2, 3, 'N/A'], key=f"Rating_{category}_{location}")
+                comment = col3a.text_input('comment.', '', key=f"comment_{category}_{location}")
+                responsible_person = col4a.selectbox('Select Responsible Person.', [''] + repair_personnel, key=f"person_{category}_{location}")
+                uploaded_file = st.file_uploader(f"Upload Images {category}", type=["jpg", "jpeg", "png"], key=f"image_{category}_{location}")
+                if st.button(f'Add {category}', key=f"add_{category}_{location}"):
+                    # تعيين رقم الحدث بناءً على التقييم
+                    if Rating in [0, 'N/A']:
+                        event_id = 'check'
+                    else:
+                        event_id = get_next_event_id()
+    
+                    image_path = ""
+                    if uploaded_file is not None:
+                        try:
+                            image = Image.open(uploaded_file)
+                            if image.mode == "RGBA":
+                                image = image.convert("RGB")
+                            max_size = (800, 600)
+                            image.thumbnail(max_size)
+                            image_filename = os.path.join('uploaded_images', f"{event_id}.jpg") if event_id else os.path.join('uploaded_images', f"no_id_{uploaded_file.name}")
+                            image.save(image_filename, optimize=True, quality=85)
+                            image_path = image_filename
+                            st.success(f"Image saved successfully as {uploaded_file.name}")
+                        except Exception as e:
+                            st.error(f"An error occurred while saving the image: {str(e)}")
+                            image_path = ""
+    
+                    new_row = {
+                        'event id': event_id,
+                        'location': location,
+                        'Element': category,
+                        'Event Detector Name': Event_Detector_Name,
+                        'Date': datetime.now().replace(tzinfo=None),
+                        'Rating': Rating,
+                        'comment': comment,
+                        'responsible person': responsible_person,
+                        'Expected repair Date': '',
+                        'Actual Repair Date': '',
+                        'image path': image_path
+                    }
+    
+                    new_row_df = pd.DataFrame([new_row])
+                    st.session_state.checklist_df = pd.concat([st.session_state.checklist_df, new_row_df], ignore_index=True)
+                    st.session_state.checklist_df.to_csv('checklist_records.csv', encoding='utf-8', index=False)
+                    st.success(f"Event recorded successfully! '{category}'!")
+    
+    # إنشاء تبويبات للجزء الثاني
+    tabs2 = st.tabs(locations_part2)
+    for location, tab in zip(locations_part2, tabs2):
+        with tab:
+            st.subheader(f'{location} Checklist.')
+            for category, items in checklist_items.items():
+                st.markdown(f"<h3 style='color:green; font-size:24px;'>{category}.</h3>", unsafe_allow_html=True)
+                for item in items:
+                    st.markdown(f"<span style='color:blue; font-size:18px;'>* {item}</span>", unsafe_allow_html=True)
+    
+                col1a, col2a, col3a, col4a = st.columns([1, 2, 2, 2])
+                Event_Detector_Name = col2a.text_input('Detector Name.', key=f"detector_name_{category}_{location}")
+                Rating = col1a.selectbox('Rating.', [0, 1, 2, 3, 'N/A'], key=f"Rating_{category}_{location}")
+                comment = col3a.text_input('comment.', '', key=f"comment_{category}_{location}")
+                responsible_person = col4a.selectbox('Select Responsible Person.', [''] + repair_personnel, key=f"person_{category}_{location}")
+                uploaded_file = st.file_uploader(f"Upload Images {category}", type=["jpg", "jpeg", "png"], key=f"image_{category}_{location}")
+                if st.button(f'Add {category}', key=f"add_{category}_{location}"):
+                    # تعيين رقم الحدث بناءً على التقييم
+                    if Rating in [0, 'N/A']:
+                        event_id = 'check'
+                    else:
+                        event_id = get_next_event_id()
+    
+                    image_path = ""
+                    if uploaded_file is not None:
+                        try:
+                            image = Image.open(uploaded_file)
+                            if image.mode == "RGBA":
+                                image = image.convert("RGB")
+                            max_size = (800, 600)
+                            image.thumbnail(max_size)
+                            image_filename = os.path.join('uploaded_images', f"{event_id}.jpg") if event_id else os.path.join('uploaded_images', f"no_id_{uploaded_file.name}")
+                            image.save(image_filename, optimize=True, quality=85)
+                            image_path = image_filename
+                            st.success(f"Image saved successfully as {uploaded_file.name}")
+                        except Exception as e:
+                            st.error(f"An error occurred while saving the image: {str(e)}")
+                            image_path = ""
+    
+                    new_row = {
+                        'event id': event_id,
+                        'location': location,
+                        'Element': category,
+                        'Event Detector Name': Event_Detector_Name,
+                        'Date': datetime.now().replace(tzinfo=None),
+                        'Rating': Rating,
+                        'comment': comment,
+                        'responsible person': responsible_person,
+                        'Expected repair Date': '',
+                        'Actual Repair Date': '',
+                        'image path': image_path
+                    }
+    
+                    new_row_df = pd.DataFrame([new_row])
+                    st.session_state.checklist_df = pd.concat([st.session_state.checklist_df, new_row_df], ignore_index=True)
+                    st.session_state.checklist_df.to_csv('checklist_records.csv', encoding='utf-8', index=False)
+                    st.success(f"Event recorded successfully! '{category}'!")
 
     with col2:
         st.markdown("""

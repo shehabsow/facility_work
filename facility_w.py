0d64a22adfeae15fd4fec3f1bb13ea7575d7b6f7
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-09-04 14:00:06 +0300

    Update facility_w.py

--- facility_w.py
+++ facility_w.py
@@ -213,16 +213,7 @@ if page == 'Event Logging':
             responsible_person = col4a.selectbox('Select Responsible Person.', [''] + repair_personnel, key=f"person_{category}_{selected_location}")
             uploaded_file = st.file_uploader(f"Upload Images {category}", type=["jpg", "jpeg", "png"], key=f"image_{category}_{selected_location}")
             if Rating in [1, 2, 3]:
-                st.markdown("""
-<style>
-    .checkbox-label {
-        font-size: 20px; /* حجم الخط أكبر */
-    }
-</style>
-""", unsafe_allow_html=True)
-
-# إنشاء مربع اختيار مع تخصيص الحجم
-            risk = st.checkbox('<span class="checkbox-label">Is this a high risk?</span>', unsafe_allow_html=True)
+                risk = st.checkbox('')
             if st.button(f'Add {category}', key=f"add_{category}_{selected_location}"):
                 if Rating in [0, 'N/A']:
                     event_id = 'check'
